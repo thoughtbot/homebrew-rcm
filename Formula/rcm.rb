7a5041cad92f2f5bcdcdbc2c2bee4ffa1c0cb636
Mike Burns <mike@mike-burns.com>
2013-09-12 20:25:40 +0200

    Bump to 1.1.0

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rcm < Formula
   homepage 'http://mike-burns.github.io/rcm'
-  url 'https://github.com/mike-burns/rcm/archive/v1.0.0.tar.gz'
-  sha1 'a5297c9d82cb58c6f897ad0ae117a0068a4ca2fa'
+  url 'https://github.com/mike-burns/rcm/archive/v1.1.0.tar.gz'
+  sha1 '66d91635405d178a3f405df3e8198b62'
 
   depends_on "autoconf"
   depends_on "automake"
