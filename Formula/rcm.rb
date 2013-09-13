89cf73d9f3795ab6f1e7469110659f889a6d602f
Mike Burns <mike@mike-burns.com>
2013-09-13 11:00:21 +0200

    Recompute the SHA1 for release 1.1.0
    Not sure how they're computing this.

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Rcm < Formula
   homepage 'http://mike-burns.github.io/rcm'
   url 'https://github.com/mike-burns/rcm/archive/v1.1.0.tar.gz'
-  sha1 '66d91635405d178a3f405df3e8198b62'
+  sha1 '68e527a4a3e7b4cc953315e711153f151b56f835'
 
   depends_on "autoconf"
   depends_on "automake"
