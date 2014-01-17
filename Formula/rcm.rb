bea4b562751b3b39fb197e616a303acb7b803e4b
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-01-17 16:12:29 -0500

    Link to thoughtbot repo, not mike-burns'

--- Formula/rcm.rb
+++ Formula/rcm.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Rcm < Formula
-  homepage 'http://mike-burns.github.io/rcm'
-  url 'https://github.com/mike-burns/rcm/archive/v1.1.0.tar.gz'
+  homepage 'http://thoughtbot.github.io/rcm'
+  url 'https://github.com/thoughtbot/rcm/archive/v1.1.0.tar.gz'
   sha1 '68e527a4a3e7b4cc953315e711153f151b56f835'
 
   depends_on "autoconf"
